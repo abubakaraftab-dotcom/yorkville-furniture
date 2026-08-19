32eed3f647287b373b68d53691224d434bda8a73
abubakaraftab-dotcom <3.05757532e+08+abubakaraftab-dotcom@users.noreply.github.com>
2026-08-19 20:19:54 +0000

    Shrink header logo size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
           {/* Logo */}
 
           <Link href="/" className="flex items-center gap-2">
-            <img src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/logo-clean-transparent.png`} alt="Yorkville Furniture Logo" className="h-14 w-auto shrink-0 object-contain" />
+            <img src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/logo-clean-transparent.png`} alt="Yorkville Furniture Logo" className="h-11 w-auto shrink-0 object-contain" />
           </Link>
 
           {/* Desktop Nav */}
